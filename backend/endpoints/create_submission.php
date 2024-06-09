b1475a5437753fe900235303ab3eab832cf84661
Shahriar Malik <shahriarnayeem8@gmail.com>
2024-06-09 23:26:30 +0200

    Improve API response messages

--- backend/endpoints/create_submission.php
+++ backend/endpoints/create_submission.php
@@ -14,13 +14,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
+    // Debugging: Display received inputs
+    // echo "Received inputs: Name = $name, Email = $email, Message = $message\n";
+
     // Read the SQL file
-    $sql = file_get_contents('../sql/insert_submission.sql');
+    $sql = file_get_contents('../sql/contact_form_submissions.sql');
     if ($sql === false) {
         http_response_code(500);
         echo "Error: Unable to read the SQL file.";
         exit();
     }
+
+    // Debugging: Display SQL content
+    // echo "SQL Content: $sql\n";
 
     // Prepare and execute the SQL statement
     $result = pg_query_params($conn, $sql, array($name, $email, $message));
@@ -28,10 +34,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result) {
         // Send a response back to the client
         http_response_code(200);
-        echo "Hi! Thank you! Your message has been received.";
+        echo json_encode([
+            "message"=> "Thank you! Your message has been received."
+        ]);
     } else {
         http_response_code(500);
-        echo "Error: Failed to insert data into the database.";
+        echo json_encode([
+            "message"=> "Something went wrong."
+        ]);
     }
 
     // Close the database connection
@@ -39,6 +49,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     // Handle invalid request methods
     http_response_code(405);
-    echo "Invalid request method.";
+    echo json_encode([
+        "message"=> "Invalid request."
+    ]);
 }
 ?>
--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '29aff1e9d46c8b5f5711e41ffd40dae29c9d3f7b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '29aff1e9d46c8b5f5711e41ffd40dae29c9d3f7b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
